a726684ba5354acef01720134b2764bc33a6f63c
bbabic <user@WS-BENJAMINBA.mistral.ba>
2017-12-10 21:34:41 +0100

    Added validation global messages

--- resources/views/partials/mainform.blade.php
+++ resources/views/partials/mainform.blade.php
@@ -1,4 +1,18 @@
 <section class="hero is-fullheight">
+  @if (session('status'))
+    <div class="notification is-warning">
+      {!! session('status') !!}
+    </div>
+  @endif
+  @if (count($errors) > 0)
+    <div class="notification is-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
   <div class="hero__bg"></div>
   <div class="hero__overlay"></div>
   <div class="hero-head">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-  @if (session('status'))
-        <div class="notification is-warning">
-          {!! session('status') !!}
-        </div>
-      @endif
-      @if (count($errors) > 0)
-        <div class="notification is-danger">
-          <ul>
-            @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-            @endforeach
-          </ul>
-        </div>
-      @endif
-
+  
       @include('partials.mainform')
       
 @endsection
